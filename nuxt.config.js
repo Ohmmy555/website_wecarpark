bf4e805ae6dda5d26b66dda036e7e9fb52a226d8
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-11-24 04:18:03 +0700

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'wecarpark',
+        title: 'WeCarpark - Easy Park Easy Pay',
         htmlAttrs: {
             lang: 'en'
         },
