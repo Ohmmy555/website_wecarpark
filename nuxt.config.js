14beefa2ea7cfbc17d6c97551d443a5000c1f2b6
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-11-24 04:26:12 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ export default {
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
             { href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC", crossorigin: "anonymous" }
         ]
     },
